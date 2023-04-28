1ee5a7edd41d66ed53a49ac95c057256cf68ee2b
moraloamg@gmail.com <moraloamg@gmail.com>
2023-04-28 11:36:21 +0200

    cambiada version API a 30

---
+++ InfoAugustobriga/app/src/main/java/proyecto/Interfaces/IModosClaroOscuro.java
@@ -0,0 +1,8 @@
+package proyecto.Interfaces;
+
+public interface IModosClaroOscuro {
+    //Constantes de modo claro y oscuro
+    //los modos de la clase Configuration no funcionan bien, por eso los pongo manualmente
+    int MODO_CLARO = 17;
+    int MODO_OSCURO = 33;
+}
